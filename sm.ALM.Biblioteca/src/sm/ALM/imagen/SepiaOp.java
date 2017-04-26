5a7eba762df6150f05557aec3affd40ba3a8eea0
Antoniolm <codiak19@gmail.com>
2017-04-26 21:42:58 +0200

    Added a new class to calculate the sepia filter

---
+++ sm.ALM.Biblioteca/src/sm/ALM/imagen/SepiaOp.java
@@ -0,0 +1,59 @@
+// *********************************************************************
+// **
+// ** Copyright (C) 2017 Antonio David López Machado
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+package sm.ALM.imagen;
+
+import java.awt.image.BufferedImage;
+import sm.image.BufferedImageOpAdapter;
+
+/**
+ *
+ * @author Antonio david lopez machado
+ */
+public class SepiaOp extends BufferedImageOpAdapter{
+    public SepiaOp () {
+     }
+    
+    public BufferedImage filter(BufferedImage src, BufferedImage dest){
+        if (src == null) {
+            throw new NullPointerException("src image is null");
+        }
+        if (dest == null) {
+            dest = createCompatibleDestImage(src, null);
+        }
+
+        for (int x = 0; x < src.getWidth(); x++) {
+            for (int y = 0; y < src.getHeight(); y++) {
+                int currentRGB=src.getRGB(x, y);
+                
+                int R= (currentRGB >> 16) & 0xFF;
+                int G= (currentRGB >> 8 ) & 0xFF;
+                int B= (currentRGB) & 0xFF;
+                
+                int sepiaR = (int) Math.min(255 ,0.393*R + 0.769*G + 0.189*B);
+                int sepiaG = (int) Math.min(255, 0.349*R + 0.686*G + 0.168*B);
+                int sepiaB = (int) Math.min(255, 0.272*R + 0.534*G + 0.131*B);
+                
+                int newRGB = (sepiaR << 16) | (sepiaG << 8) | sepiaB; 
+                dest.setRGB(x, y, newRGB);
+            }
+        }
+        return dest;
+    }
+    
+}
