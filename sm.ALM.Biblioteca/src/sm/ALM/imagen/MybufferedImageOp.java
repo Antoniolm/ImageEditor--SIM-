0e3cdaed51acd5e08fe52255930b86eff60d68d2
Antoniolm <codiak19@gmail.com>
2017-05-28 19:01:15 +0200

    A few changes in our own filter

--- sm.ALM.Biblioteca/src/sm/ALM/imagen/MybufferedImageOp.java
+++ sm.ALM.Biblioteca/src/sm/ALM/imagen/MybufferedImageOp.java
@@ -24,6 +24,8 @@ public class MybufferedImageOp extends BufferedImageOpAdapter{
             dest = createCompatibleDestImage(src, null);
         }
 
+        int mediaR=0,mediaG=0,mediaB=0;
+        int nPixel=0;
         for (int x = 0; x < src.getWidth(); x++) {
             for (int y = 0; y < src.getHeight(); y++) {
                 int currentRGB=src.getRGB(x, y);
@@ -32,12 +34,36 @@ public class MybufferedImageOp extends BufferedImageOpAdapter{
                 int G= (currentRGB >> 8 ) & 0xFF;
                 int B= (currentRGB) & 0xFF;
                 
-                G=(R+G)%255;
-                B=(G+B)%255;
-                
-                dest.setRGB(x, y, (R << 16) | (G << 8) | B);
+                mediaR+=R;
+                mediaG+=G;
+                mediaB+=B;
+                nPixel++;
             }
         }
+        
+        mediaR/=nPixel;
+        mediaG/=nPixel;
+        mediaB/=nPixel;
+        
+        int umbralMedia=(mediaR+mediaG+mediaB)/3;
+        
+        for (int x = 0; x < src.getWidth(); x++) {
+            for (int y = 0; y < src.getHeight(); y++) {
+                int currentRGB=src.getRGB(x, y);
+                
+                int R= (currentRGB >> 16) & 0xFF;
+                int G= (currentRGB >> 8 ) & 0xFF;
+                int B= (currentRGB) & 0xFF;
+                
+                if((R+G+B)/3>=umbralMedia){
+                    dest.setRGB(x, y, (R << 16) | (G << 8) | B);
+                }
+                else{
+                    dest.setRGB(x, y, (0 << 16) | (0 << 8) | 0);
+                }
+            }
+        }
+        
         return dest;
     } 
 }
